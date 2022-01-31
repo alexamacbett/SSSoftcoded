3cbb46dd054b8485086be0cf9078c03e1af9ec1b
Alexa MacBett <alexmacbett@gmail.com>
2022-01-31 02:10:42 +0000

    Fixed loading screen loading to fall back on/overwrite the defaults in the correct way

--- Plugin.cs
+++ Plugin.cs
@@ -35,28 +35,34 @@ namespace SSSoftcoded
             GameObject gameObject = GameObject.Find("Wallpaper");
             //The numbers 1-10 in the folder will overwrite the 1-10 loading screens in the asset. All other filenames will be added to the list.
             //If Zubmariner is installed, 11-14 will overwrite. If not, they will add.
-            int maxKnownLoadingScreens = GameProvider.Instance.IsZubmariner ? 14 : 10;
-            //List<string> loadingScreens = LoadingHelper.GetLoadingScreens();
-            List<string> loadingScreens = new List<string>();
-            int firstAssetScreen = loadingScreens.Count; //First of the original
-            for (int i = 1; i <= maxKnownLoadingScreens; i++)
+            List<string> knownLoadingScreens = GameProvider.Instance.IsZubmariner ? new List<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14" } : new List<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+            List<LoadableResource> loadingScreens = new List<LoadableResource>();
+            foreach (string screen in LoadingHelper.GetLoadingScreens())
             {
-                string prospectiveFilename = LoadingHelper.GetLoadingScreenFilePath() + i.ToString();
-                if (!loadingScreens.Contains(prospectiveFilename))
+                if (knownLoadingScreens.Contains(screen))
                 {
-                    loadingScreens.Add(i.ToString());
+                    loadingScreens.Add(new LoadableResource(screen, true, true));
+                    knownLoadingScreens.Remove(screen);
                 }
+                else
+                {
+                    loadingScreens.Add(new LoadableResource(screen, false, true));
+                }
+            }
+            foreach(string screen in knownLoadingScreens)
+            {
+                loadingScreens.Add(new LoadableResource(screen, true, false));
             }
             System.Console.WriteLine("Loaded " + loadingScreens.Count + " loading screen backgrounds to choose from.");
             int chosenLoadingScreen = UnityEngine.Random.Range(0, loadingScreens.Count - 1);
             Texture2D texture2D;
-            //if (chosenLoadingScreen < firstAssetScreen)
-            //{
-                texture2D = LoadingHelper.GetBackgroundTexture(loadingScreens[chosenLoadingScreen]);
-            /*} else
-            //{
-            //    texture2D = UnityEngine.Resources.Load(loadingScreens[chosenLoadingScreen]) as Texture2D;
-            }*/
+            if (loadingScreens[chosenLoadingScreen].GetFileResource())
+            {
+                texture2D = LoadingHelper.GetBackgroundTexture(loadingScreens[chosenLoadingScreen].GetName());
+            } else
+            {
+                texture2D = UnityEngine.Resources.Load(LoadingHelper.GetLoadingScreenAssetPath() + loadingScreens[chosenLoadingScreen].GetName()) as Texture2D;
+            }
             System.Console.WriteLine("All good before assigning texture.");
             gameObject.GetComponent<RawImage>().texture = (Texture)texture2D;
             GameObject.Find("Tip").GetComponent<Text>().text = StaticEntities.LoadingScreenTips[UnityEngine.Random.Range(0, ((IEnumerable<string>)StaticEntities.LoadingScreenTips).Count<string>())];
@@ -98,6 +104,50 @@ namespace SSSoftcoded
         public static string GetLoadingScreenAssetPath()
         {
             return loadingScreenAssetPath;
+        }
+    }
+
+    public class LoadableResource
+    {
+        private string _name = "";
+        private bool _isAssetResource = false;
+        private bool _isFileResource = false; 
+
+        public LoadableResource(string name, bool isAssetResource, bool isFileResource)
+        {
+            _name = name;
+            _isAssetResource = isAssetResource;
+            _isFileResource = isFileResource;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+
+        public bool GetAssetResource()
+        {
+            return _isAssetResource;
+        }
+
+        public bool GetFileResource()
+        {
+            return _isFileResource;
+        }
+
+        public void SetAssetResource(bool state)
+        {
+            _isAssetResource = state;
+        }
+
+        public void SetFileResource(bool state)
+        {
+            _isFileResource = state;
         }
     }
 }
